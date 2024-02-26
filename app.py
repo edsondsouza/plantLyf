ba356c94b66dda1b0d147f5d8902f1fb7377761b
edson <edsondsouzadev@gmail.com>
2024-02-26 20:11:34 +0530

    update about section and form added for crop recommendation

--- app.py
+++ app.py
@@ -2,18 +2,19 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import firebase_admin
 from firebase_admin import auth 
 app = Flask(__name__)
+
+# signup page
 @app.route('/')
 def main_p():
     # Render your main page template here
     return render_template("/signup.html")
 
+# signin page
 @app.route("/signin") #here
 def signin():
-
      if request.method == "POST":
          email = request.form.get("email")
          password = request.form.get("password")
-
          try:
              user = firebase_admin.auth.sign_in_with_email_and_password(email, password)
              # Handle sign-in success, update user data in database, redirect, etc.
@@ -30,21 +31,29 @@ def signin():
          except firebase_admin.auth.AuthError as e:
              # Handle other authentication errors
              pass
-
      # Redirect to sign-in page if sign-in fails
      return render_template("/signin.html")
 
+# main page
 @app.route("/main")
 def main_page():
     # Render your main page template here
     return render_template("/main.html")
 
+# crop recommendation page
 @app.route('/croprecommendation')
 def recommendation():
     return render_template('plantRec.html')
+
+# disease detection page
 @app.route('/disease_detection')
 def detection():
     return render_template('diseaseDetect.html')
+
+# about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
